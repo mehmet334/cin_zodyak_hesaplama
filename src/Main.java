d58fde08914c62d89a25258fc4c39660305b6343
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-08 14:44:33 +0300

    güncelleme

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner =new Scanner(System.in);
+        String metin="";
+        System.out.println("Doğum Tarihinizi Giriniz: ");
+        int tarih = scanner.nextInt();
+        int artik= tarih%12;
+        if (artik==0){
+            metin ="Maymun";
+        } else if (artik==1) {
+            metin="Horoz";
+        } else if (artik==2) {
+            metin="Köpek";
+        } else if (artik==3) {
+            metin="Domuz";
+        } else if (artik==4) {
+            metin="Fare";
+        } else if (artik==5) {
+            metin="Öküz";
+        } else if (artik==6) {
+            metin="Kaplan";
+        } else if (artik==7) {
+            metin="Tavşan";
+        } else if (artik==8) {
+            metin="Ejderha";
+        } else if (artik==9) {
+            metin="Yılan";
+        } else if (artik==10) {
+            metin="At";
+        } else if (artik==11) {
+            metin="Koyun";
         }
+
+        System.out.println("Çin Zodyağı Burcunuz: " + metin);
+
     }
 }
